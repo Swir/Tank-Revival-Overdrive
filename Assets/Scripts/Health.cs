1bafa03a7fba2acb10c3c3a5ba7cd08c868435e3
Jan Szczerbaty Uśmiechnięty <swirjestem@hotmail.com>
2026-09-10 21:16:57 +0200

    Support persistent current health for defended eagle

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -21,11 +21,11 @@ namespace TankRevival
         public Action<Health> Died;
         public Action<Health, int> Damaged;
 
-        public void Initialize(Team team, int maxHealth)
+        public void Initialize(Team team, int maxHealth, int currentHealth = -1)
         {
             Team = team;
             Maximum = Mathf.Max(1, maxHealth);
-            Current = Maximum;
+            Current = currentHealth < 0 ? Maximum : Mathf.Clamp(currentHealth, 1, Maximum);
             IsDead = false;
         }
 
